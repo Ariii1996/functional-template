b20023d88145349ba73692aa5155bd431d9e0b78
Ariel <ariel.piro@hotmail.com>
2020-11-21 21:07:02 -0300

    exercise 10 done

--- src/exercise10.clj
+++ src/exercise10.clj
@@ -1,21 +1,18 @@
 (ns exercise10)
 
-(def accounts #{})
-(def id-account 0)
-
 (defn open-account []
-  ()
+  ((constantly 0) 0)
 )
 
 (defn close-account [account]
-( )
+( (constantly nil) 0)
 )
 
 (defn get-balance [account]
-  ()
+  ((constantly account) 0)
 )
 
 (defn update-balance [account amount]
-  ()
+  (+ account amount)
 )
 
